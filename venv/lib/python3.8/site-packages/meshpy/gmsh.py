3c365ddfef89bbb7dd946ce1c9c1fe4bda8e4d39
Tetsuo Koyama <tkoyama010@gmail.com>
2020-10-17 10:18:54 +0900

    :wastebasket: venv

--- venv/lib/python3.8/site-packages/meshpy/gmsh.py
+++
@@ -1,4 +0,0 @@
-from gmsh_interop.runner import *
-
-from warnings import warn
-warn("meshpy.gmsh is deprecated. Use gmsh_interop.runner instead.", DeprecationWarning)
--- venv/lib/python3.8/site-packages/meshpy/gmsh_reader.py
+++
@@ -1,4 +0,0 @@
-from gmsh_interop.reader import *
-
-from warnings import warn
-warn("meshpy.gmsh_reader is deprecated. Use gmsh_interop.reader instead.", DeprecationWarning)
